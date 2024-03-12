c1bc4a3e8ed168a4eed9e2e7ca69fa604c7d1f85
Olori-Odo <ojuotimi04@gmail.com>
2024-03-12 13:54:08 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react'
+import { useState } from 'react'
 
 import './App.css'
 import Arithmetic from './components/Arithmetic'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
--- src/components/Arithmetic.jsx
+++ src/components/Arithmetic.jsx
@@ -3,16 +3,33 @@ import { useState } from "react"
 
 const Arithmetic = ()=>{
     const [count, setCount] = useState(0)
+
+    // const add = ()=> {
+    //     return setCount(count = 1)
+    // }
+
+    // const sub = ()=> {
+    //     return (count > 0) ? setCount(count - 1) : 0
+    // }
     return(
         <>
             <div className="func">
 
             
-                <button>Addition</button>
+                <button onClick={()=> setCount((count + 1))}>Addition</button>
                 <br />
                 <h1> {count} </h1>
                 <br />
-                <button>Subtraction</button>
+                <button onClick={()=>{
+                    if (count > 0) {
+                        return( setCount((count - 1)))
+                        
+                    }
+                    
+                  
+                
+                    
+                }}>Subtraction</button>
 
             </div>
         </>
